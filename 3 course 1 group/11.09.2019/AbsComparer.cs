48eb88519194bab139728126370846dd33b28460
AnzhelikaKravchuk <anzhelika.kravchuk@gmail.com>
2019-09-11 21:41:13 +0300

    Add new folder

---
+++ 3 course 1 group/11.09.2019/AbsComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class AbsComparer
+    {
+        public int Compare(int lhs, int rhs) 
+            => Math.Abs(lhs) - Math.Abs(rhs);
+    }
+
+    public class LengthComparer
+    {
+        public int Compare(int lhs, int rhs)
+            => lhs.ToString().Length - rhs.ToString().Length;
+    }
+}
