ed4357942c0d5fc710dc238742130dcabe7ebe64
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2019-09-11 22:09:26 +0300

    Update ArrayExtension.cs

--- 3 course 1 group/11.09.2019/ArrayExtension.cs
+++ 3 course 1 group/11.09.2019/ArrayExtension.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
-    public class Class1
+    public static class ArrayExtension
     {
+        public static void BubbleSort(int[] array)
+        {
+            AbsComparer comparer = new AbsComparer();
+
+            if (array is null)
+            {
+                throw new ArgumentNullException($"{nameof(array)} can not be null!");
+            }
+
+            if (array.Length == 0)
+            {
+                return;
+            }
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (comparer.Compare(array[j], array[j + 1]) <= 0
+                    {
+
+                    }
+                }
+            }
+
+
+        }
+
+        public static void MergeSort(int[] array)
+        {
+
+        }
+
+        public static void QuickSort(int[] array)
+        {
+
+        }
     }
 }
